86f559eafdd016113a7315b539457287550e3b59
FarshidABZ <farshid.abazari@sigmasoftwares.com>
2020-07-11 23:53:07 +0300

    Fix data base and version code

--- app/src/main/java/ir/kindnesswall/data/local/dao/AppDatabase.kt
+++ app/src/main/java/ir/kindnesswall/data/local/dao/AppDatabase.kt
@@ -19,7 +19,7 @@ import ir.kindnesswall.data.local.dao.submitrequest.RegisterGiftRequestModel
         CharityModel::class,
         GiftModel::class,
         RegisterGiftRequestModel::class],
-    version = 7
+    version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun provinceDao(): ProvinceDao
