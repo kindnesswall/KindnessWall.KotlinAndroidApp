c803c82d3749155fd9979b5eaee4a134add2c35a
mohammadreza jafari <mrjafari483@Gmail.com>
2021-09-08 06:15:16 -0700

    handle null value phone visibility setting

--- app/src/main/java/ir/kindnesswall/utils/NumberStatus.kt
+++ app/src/main/java/ir/kindnesswall/utils/NumberStatus.kt
@@ -2,6 +2,7 @@ package ir.kindnesswall.utils
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import android.view.View
 import android.widget.RadioButton
 import androidx.lifecycle.LifecycleOwner
@@ -12,60 +13,77 @@ import ir.kindnesswall.view.main.addproduct.SubmitGiftViewModel
 import kotlinx.android.synthetic.main.activity_submit_gift.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class NumberStatus (val viewModel: SubmitGiftViewModel,val none : RadioButton ,val charity : RadioButton ,val all : RadioButton  ) {
-    var shPref: SharedPreferences?=null
+class NumberStatus(
+    val viewModel: SubmitGiftViewModel,
+    val none: RadioButton,
+    val charity: RadioButton,
+    val all: RadioButton
+) {
+    var shPref: SharedPreferences? = null
     val keyName = "nameKey"
-    var sEdite : SharedPreferences.Editor ?=null
-    var context : Context ? = null
-    fun getShowNumberStatus(context: Context){
+    var sEdite: SharedPreferences.Editor? = null
+    var context: Context? = null
+    fun getShowNumberStatus(context: Context) {
         this.context = context
-        shPref= context.getSharedPreferences(UserInfoPref.MyPref, Context.MODE_PRIVATE)
-        sEdite =shPref!!.edit()
-        if (shPref!!.contains(keyName)){
+        shPref = context.getSharedPreferences(UserInfoPref.MyPref, Context.MODE_PRIVATE)
+        sEdite = shPref!!.edit()
+        if (shPref!!.contains(keyName)) {
             fromSharedPreferences()
-        }else{
+        } else {
             fromServer()
         }
     }
 
-    private fun fromSharedPreferences(){
-        when(shPref!!.getString(keyName, null)){
-            "none"->{
+    private fun fromSharedPreferences() {
+        when (shPref!!.getString(keyName, null)) {
+            "none" -> {
                 none.isChecked = true
             }
-            "charity"->{
-                charity.isChecked= true
+            "charity" -> {
+                charity.isChecked = true
             }
-            "all"->{
-                all.isChecked= true
+            "all" -> {
+                all.isChecked = true
             }
         }
     }
-    private fun fromServer(){
-        viewModel.getPhoneVisibility().observe(context as LifecycleOwner){
-            when(it.status){
-                CustomResult.Status.LOADING ->{
+
+    private fun fromServer() {
+        viewModel.getPhoneVisibility().observe(context as LifecycleOwner) {
+            when (it.status) {
+                CustomResult.Status.LOADING -> {
 
                 }
-                CustomResult.Status.SUCCESS->{
-                    when(it.data!!.setting){
-                        "none"->{
-                            none.isChecked = true
+                CustomResult.Status.SUCCESS -> {
+                    if (it.data?.setting == null) {
+                        none.isChecked = true
+                        editSharedPreferences("none")
+                    } else {
+                        when (it.data!!.setting) {
+                            "none" -> {
+                                none.isChecked = true
+                            }
+                            "charity" -> {
+                                charity.isChecked = true
+                            }
+                            "all" -> {
+                                all.isChecked = true
+                            }
                         }
-                        "charity"->{
-                            charity.isChecked= true
-                        }
-                        "all"->{
-                            all.isChecked= true
-                        }
+                        editSharedPreferences(it.data!!.setting.toString())
+
                     }
-                    sEdite!!.putString(keyName,it.data!!.setting.toString())
-                    sEdite!!.apply()
+
+
                 }
-                CustomResult.Status.ERROR->{
+                CustomResult.Status.ERROR -> {
 
                 }
             }
         }
+    }
+    fun editSharedPreferences(value:String){
+        sEdite!!.putString(keyName, value)
+        sEdite!!.apply()
     }
 }
