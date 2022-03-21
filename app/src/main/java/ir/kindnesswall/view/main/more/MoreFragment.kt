5a24ce2f514ff3afe080561635ab470eefe2dba3
Farhad Beigirad <f.beigirad@gmail.com>
2022-03-21 21:50:25 +0330

    add a dummy crash for testing crash report

--- app/src/main/java/ir/kindnesswall/view/main/more/MoreFragment.kt
+++ app/src/main/java/ir/kindnesswall/view/main/more/MoreFragment.kt
@@ -5,9 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -17,6 +28,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -70,6 +82,7 @@ class MoreFragment : BaseFragment() {
 
     override fun configureViews() {}
 
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         view as ComposeView
         view.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
@@ -173,7 +186,16 @@ class MoreFragment : BaseFragment() {
                         Text(
                             text = stringResource(R.string.version, BuildConfig.VERSION_NAME),
                             style = MaterialTheme.typography.overline,
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .combinedClickable(
+                                    enabled = BuildConfig.FLAVOR == "staging",
+                                    interactionSource = remember { MutableInteractionSource() },
+                                    indication = null,
+                                    onClick = {},
+                                    onLongClick = { throwByLongClick() },
+                                    onDoubleClick = { throwByDoubleClick() }
+                                )
                         )
                     }
                 }
@@ -222,6 +244,20 @@ class MoreFragment : BaseFragment() {
                 dialog.cancel()
             }
             .show()
+    }
+
+    private fun throwByLongClick() {
+        throw RuntimeException("Hello from more fragment: long click")
+    }
+
+    private fun throwByDoubleClick() {
+        TestCrash().crash("Hello from more fragment: double click")
+    }
+
+    class TestCrash {
+        fun crash(message: String) {
+            throw RuntimeException(message)
+        }
     }
 }
 
