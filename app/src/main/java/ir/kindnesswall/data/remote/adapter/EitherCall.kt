726cf73d9bef9633ee37c8410505a8220fefe476
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-09-05 23:28:57 +0430

    improve EitherCall.kt

--- app/src/main/java/ir/kindnesswall/data/remote/adapter/EitherCall.kt
+++ app/src/main/java/ir/kindnesswall/data/remote/adapter/EitherCall.kt
@@ -3,12 +3,14 @@ package ir.kindnesswall.data.remote.adapter
 import ir.kindnesswall.data.model.Either
 import ir.kindnesswall.data.model.Error
 import okhttp3.Request
-import retrofit2.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.lang.reflect.Type
 
 internal class EitherCall<R>(
     private val delegate: Call<R>,
-    private val successType: Type
+    private val successType: Type,
 ) : Call<Either<Error, R>> {
 
     override fun enqueue(callback: Callback<Either<Error, R>>) {
@@ -16,27 +18,10 @@ internal class EitherCall<R>(
         delegate.enqueue(object : Callback<R> {
             override fun onResponse(
                 call: Call<R>,
-                response: Response<R>
+                response: Response<R>,
             ) {
-                callback.onResponse(this@EitherCall, Response.success(response.toEither()))
-            }
-
-            private fun Response<R>.toEither(): Either<Error, R> {
-                if (!isSuccessful) {
-                    errorBody()?.let {
-                        return Either.Left(
-                            Error(message(), code(), errorBody().toString())
-                        )
-                    }
-                }
-                body()?.let { body -> return Either.Right(body) }
-                return if (successType == Unit::class.java) {
-                    @Suppress("UNCHECKED_CAST")
-                    Either.Right(Unit) as Either<Error, R>
-                } else {
-                    @Suppress("UNCHECKED_CAST")
-                    Either.Left(UnknownError("Response body was null")) as Either<Error, R>
-                }
+                return callback.onResponse(this@EitherCall,
+                    Response.success(mapResponseToEither(response)))
             }
 
             override fun onFailure(call: Call<R>, throwable: Throwable) {
@@ -44,16 +29,41 @@ internal class EitherCall<R>(
                     this@EitherCall,
                     Response.success(
                         Either.Left(
-                            throwable.message?.let {
-                                Error(message = it, serverError = true)
-                            } ?: kotlin.run {
-                                Error(message = throwable.toString(), serverError = true)
-                            }
+                            Error(
+                                message = throwable.message ?: throwable.toString(),
+                                serverError = true
+                            )
                         )
                     )
                 )
             }
         })
+    }
+
+    private fun <T> mapResponseToEither(
+        response: Response<T>,
+    ): Either<Error, T> {
+        when (response.code()) {
+            204 -> {
+                @Suppress("UNCHECKED_CAST")
+                return Either.Right(Unit) as Either<Error, T>
+            }
+            in 200 until 300 -> {
+                val body = response.body()
+                return if (body != null) Either.Right(body)
+                else Either.Left(Error(message = "Response body was null"))
+            }
+            503 -> {
+                return Either.Left(Error(message = response.message(), serverError = true))
+            }
+            else -> {
+                return Either.Left(
+                    Error(response.message(),
+                        response.code(),
+                        response.errorBody().toString())
+                )
+            }
+        }
     }
 
     override fun isExecuted() = delegate.isExecuted
