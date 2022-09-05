495fdb7682a2ddfed009afe948766970616b7af2
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-09-05 23:22:00 +0430

    remove nullable right-side

--- app/src/main/java/ir/kindnesswall/data/model/Either.kt
+++ app/src/main/java/ir/kindnesswall/data/model/Either.kt
@@ -2,5 +2,5 @@ package ir.kindnesswall.data.model
 
 sealed class Either<out A, out B> {
     data class Left<out A> constructor(val left: A) : Either<A, Nothing>()
-    data class Right<out B> constructor(val right: B?) : Either<Nothing, B>()
+    data class Right<out B> constructor(val right: B) : Either<Nothing, B>()
 }
